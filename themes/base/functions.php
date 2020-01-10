1fe150d0b12e09d509fdde4c40c35a874714d0b3
Jonathan Vieira <wallacejsv@gmail.com>
2020-01-09 21:36:39 -0300

    estrutura interface e image thumb produtos

--- themes/base/functions.php
+++ themes/base/functions.php
@@ -11,3 +11,13 @@ function base_css() {
 }
 add_action('wp_enqueue_scripts', 'base_css');
 
+function base_custom_image() {
+    add_image_size('slide', 1000, 800, ['center', 'top']);
+    update_option('medium_crop', 1);
+}
+add_action('after_setup_theme', 'base_custom_image');
+
+function base_loop_shop_per_page() {
+    return 6;
+}
+add_filter('loop_shop_per_page', 'base_loop_shop_per_page');
--- themes/base/header.php
+++ themes/base/header.php
@@ -3,7 +3,33 @@
 <head>
 	<meta charset="utf-8"/>
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
-	<title><?php bloginfo('name'); ?> | <?php the_title(); ?></title>
+	<title><?php bloginfo('name'); ?> <?php wp_title('|'); ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+
+<?php 
+	$img_url = get_stylesheet_directory_uri() . '/img';
+	$cart_count = WC()->cart->get_cart_contents_count();
+?>
+
+<header class="header">
+	<a href="/"><img src="<?= $img_url; ?>/handel.svg" alt="base"></a>
+	<div class="busca">
+		<?php 
+			//get_product_search_form();
+		?>
+		<form action="<?php bloginfo('url') ?>/loja/" method="get">
+			<input type="text" name="s" id="s" placeholder="Buscar" value="<?php the_search_query(); ?>">
+			<input type="text" name="post_type" value="product" class="hidden">
+			<input type="submit" value="Buscar" id="searchbutton">
+		</form>
+	</div>
+
+	<nav class="conta">
+		<a href="/minha-conta" class="minha-conta">Minha Conta</a>
+		<a href="/carrinho" class="carrinho">Carrinho
+			<?php echo $cart_count ? '<span class="carrinho-count">'. $cart_count .'</span>' : '' ?>
+		</a>
+	</nav>
+</header>
